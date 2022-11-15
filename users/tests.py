c1402120e1d60afdbf60b6c6dbfb7b47c2c950a0
kbm1933 <wjsalsgh456@gmail.com>
2022-11-15 09:38:40 +0900

    test code_morning quiz

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
 
-# Create your tests here.
+class UserTestCase(APITestCase):
+    def test_signup(self):
+        url = reverse("user_view")
+        user_data = {
+            "username":"testuser",
+            "fullname":"tester",
+            "email":"test@test.com",
+            "password":"password",
+        }
+        response = self.client.post(url,user_data)
+        self.assertEqual(response.data,{"message": "가입 완료!!"})
